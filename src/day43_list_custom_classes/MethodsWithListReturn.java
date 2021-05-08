826e631c0c92cd45d59b6fb4270ffa0675312ca4
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-05-08 11:35:36 -0400

    MethodsWithListReturn

--- src/day43_list_custom_classes/MethodsWithListReturn.java
+++ src/day43_list_custom_classes/MethodsWithListReturn.java
@@ -1,21 +1,24 @@
 package day43_list_custom_classes;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MethodsWithListReturn {
     public static void main(String[] args) {
+        List<Integer> mlnNums = getIntegerList();
+        System.out.println(mlnNums);
 
+    }
 
+    public static List<Integer> getIntegerList() {
         List<Integer> nums = new ArrayList<>();
-        for (int i = 0; i <=1000_000 ; i++) {
+        for(int i = 0; i <= 1_000_000; i++) {
             nums.add(i);
-
-
         }
+        return nums;
+    }
 
-    }
-    public static List<Integer> getIntegarList(){
-        return null;
-    }
+
+
+
 }
+
