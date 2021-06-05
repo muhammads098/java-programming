c8a4b653f097793709cb7b1413e5de0b728d2c4b
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-06-05 12:22:06 -0400

    added some more work

--- src/day50_inheritance/StaticBlockDemo.java
+++ src/day50_inheritance/StaticBlockDemo.java
@@ -1,4 +1,4 @@
-package day50_inheritence;
+package day50_inheritance;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -77,10 +77,20 @@ public class StaticBlockDemo {
                 "Volvo"));
     }
 
+    { //initializer block -> runs each time object created, before constructor
+        System.out.println("Initializer block");
+    }
+
     public StaticBlockDemo() {
         System.out.println("Constructor method");
         num += 5;
     }
+
+    public StaticBlockDemo(int value) {
+        System.out.println("Overloaded constructor - value = " + value);
+        num += value;
+    }
+
 
     //other methods that use the carModels arraylist
 
