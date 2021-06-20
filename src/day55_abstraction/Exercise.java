4123ab078cd387c6704f5d65c6978d1a514168ff
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-06-20 11:51:57 -0400

    abstract class practice

---
+++ src/day55_abstraction/Exercise.java
@@ -0,0 +1,11 @@
+package day55_abstraction;
+
+public abstract class Exercise {
+    public void start(){ //non-abstract method
+        System.out.println("Warming up and starting the exercise ");
+    }
+    public abstract void perform(); //abstract method - method without body/implementation , just signature
+
+    public abstract int getCaloriesCount(int minutes); //this is a abstract method that concrete sub classes will override/implement
+
+}
---
+++ src/day55_abstraction/Running.java
@@ -0,0 +1,14 @@
+package day55_abstraction;
+
+public class Running extends Exercise{
+
+    @Override
+    public void perform() {
+
+    }
+
+    @Override
+    public int getCaloriesCount(int minutes) {
+        return 0;
+    }
+}
